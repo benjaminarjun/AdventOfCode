42d74eb36364f852291f466d07a452e98c5954a1
Ben <benarthur91@gmail.com>
2019-12-01 16:50:13 -0800

    Move missing file exception handling to data loading function

--- day1/part1.py
+++ day1/part1.py
@@ -5,15 +5,16 @@ from lib import get_total_fuel_requirement
 
 
 def _get_input_from_file():
-    with open('input.txt', 'r') as f:
-        input = [int(line.strip()) for line in f.readlines()]
+    try:
+        with open('input.txt', 'r') as f:
+            input = [int(line.strip()) for line in f.readlines()]
+    except FileNotFoundError as f:
+        raise Exception('Could not find file with input masses. Check working directory.', f)
+
     return input
 
 
 if __name__ == '__main__':
-    try:
-        masses = _get_input_from_file()
-    except FileNotFoundError as f:
-        raise Exception('Could not find file with input masses. Check working directory.', f)
-    
-    print(get_total_fuel_requirement(masses))
+    masses = _get_input_from_file()
+    print(f'Part 1:  {get_total_fuel_requirement(masses, include_fuel=False)}')
+    print(f'Part 2:  {get_total_fuel_requirement(masses, include_fuel=True)}')
