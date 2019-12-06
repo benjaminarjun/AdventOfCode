02b80e9f47fd2a9ff15baf5a5548a1897eb74ed6
Ben <benarthur91@gmail.com>
2019-12-06 13:22:10 -0800

    Convert day 6 to a class solution since we need to share the orbit lookup

--- python/solutions/day6/lib.py
+++ python/solutions/day6/lib.py
@@ -1,67 +1,60 @@
-def count_orbits(orbit_def):
-    orbit_lookup = _get_orbit_lookup(orbit_def)
+class OrbitGroup:
+    def __init__(self, orbits):
+        self.orbits = orbits
+        self._build_orbit_lookup(orbits)
 
-    total_orbits = 0
-    for orbiting_object in orbit_lookup:
-        current_object = orbiting_object
-        orbits_this_obj = 0
+    def count_total_orbits(self):
+        total_orbits = 0
+        for orbiting_object in self.orbit_lookup:
+            current_object = orbiting_object
+            orbits_this_obj = 0
 
-        while current_object is not None:
-            current_object = orbit_lookup.get(current_object, None)
-            if current_object is not None:
-                orbits_this_obj += 1
+            while current_object is not None:
+                current_object = self.orbit_lookup.get(current_object, None)
+                if current_object is not None:
+                    orbits_this_obj += 1
 
-        total_orbits += orbits_this_obj
+            total_orbits += orbits_this_obj
 
-    return total_orbits
+        return total_orbits
 
+    def count_orbit_transfers(self, initial_orbited, desired_orbited):
+        # Get paths from each object to COM, a known common ancestor.
+        initial_obj_path_to_com = self._get_path(initial_orbited, 'COM')
+        desired_obj_path_to_com = self._get_path(desired_orbited, 'COM')
 
-def count_orbit_transfers(orbit_def, initial_orbited, desired_orbited):
-    orbit_lookup = _get_orbit_lookup(orbit_def)
+        most_recent_common_ancestor = list(filter(
+            lambda x: x[0] == x[1],
+            zip(
+                reversed(initial_obj_path_to_com),
+                reversed(desired_obj_path_to_com)
+            )
+        ))[-1][0]
 
-    # Get paths from each object to COM, a known common ancestor.
-    initial_obj_path_to_com = _get_path(orbit_def, initial_orbited, 'COM')
-    desired_obj_path_to_com = _get_path(orbit_def, desired_orbited, 'COM')
+        return len(self._get_path(initial_orbited, most_recent_common_ancestor)) - 1\
+            + len(self._get_path(desired_orbited, most_recent_common_ancestor)) - 1
 
-    most_recent_common_ancestor = list(filter(
-        lambda x: x[0] == x[1],
-        zip(
-            reversed(initial_obj_path_to_com),
-            reversed(desired_obj_path_to_com)
-        )
-    ))[-1][0]
+    def _get_path(self, from_obj, to_obj):
+        current_obj = from_obj
+        path = [current_obj]
 
-    return len(_get_path(orbit_def, initial_orbited, most_recent_common_ancestor)) - 1\
-        + len(_get_path(orbit_def, desired_orbited, most_recent_common_ancestor)) - 1
+        while current_obj != to_obj:
+            current_obj = self.orbit_lookup.get(current_obj, None)
 
+            if current_obj is None:
+                raise ValueError(f'Object <{from_obj}> does not orbit <{to_obj}>')
+            else:
+                path.append(current_obj)
 
-def _get_path(orbit_def, from_obj, to_obj):
-    orbit_lookup = _get_orbit_lookup(orbit_def)
+        return path
 
-    current_obj = from_obj
-    path = [current_obj]
+    def _build_orbit_lookup(self, orbits):
+        # Dict to hold, for a given object, the object that that object orbits.
+        # COM won't appear in this list; we'll say it's reserved for objects that actually orbit something.
+        self.orbit_lookup = {}
 
-    while current_obj != to_obj:
-        current_obj = orbit_lookup.get(current_obj, None)
-
-        if current_obj is None:
-            raise ValueError(f'Object <{from_obj}> does not orbit <{to_obj}>')
-        else:
-            path.append(current_obj)
-
-    return path
-
-
-def _get_orbit_lookup(orbit_def):
-    # Dict to hold, for a given object, the object that that object orbits.
-    # COM won't appear in this list; we'll say it's reserved for objects that actually orbit something.
-    orbit_lookup = {}
-
-    for line in orbit_def.splitlines():
-        orbited, orbiting = line.split(')')
-        orbit_lookup[orbiting] = orbited
-
-    return orbit_lookup
+        for orbited, orbiting in orbits:
+            self.orbit_lookup[orbiting] = orbited
 
 
 # TODO would be awesome to be able to draw the diagram
--- python/solutions/day6/results.py
+++ python/solutions/day6/results.py
@@ -1,11 +1,11 @@
-from .lib import count_orbits, count_orbit_transfers
+from .lib import OrbitGroup
 from ..aoc_helpers import get_data_file
 
 
 def _get_input_from_file():
     try:
         with open(get_data_file('day6_input.txt'), 'r') as f:
-            input = f.read()
+            input = [line.strip().split(')') for line in f.readlines()]
     except FileNotFoundError as f:
         raise Exception('Could not find file', f)
 
@@ -14,7 +14,8 @@ def _get_input_from_file():
 
 if __name__ == '__main__':
     input = _get_input_from_file()
+    orbit_group = OrbitGroup(input)
 
-    print(f'Part 1:  {count_orbits(input)}')
+    print(f'Part 1:  {orbit_group.count_total_orbits()}')
     # TODO subtracting 2 because no easy way to get from YOU to the thing it orbits; same with SAN
-    print(f'Part 2:  {count_orbit_transfers(input, "YOU", "SAN") - 2}')
+    print(f'Part 2:  {orbit_group.count_orbit_transfers( "YOU", "SAN") - 2}')
