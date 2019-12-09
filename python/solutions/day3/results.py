933aac6b437f64362982ff1e3b6f875dac1634fe
Ben <benarthur91@gmail.com>
2019-12-08 23:28:32 -0800

    Working python solution for day 3

--- python/solutions/day3/results.py
+++ python/solutions/day3/results.py
@@ -2,7 +2,7 @@
 
 
 from ..aoc_helpers import get_data_file
-from .lib import get_closest_shared_point_traversed, manhattan_distance
+from .lib import get_closest_shared_point, get_min_signal_delay, manhattan_distance
 
 
 def _get_input_from_file():
@@ -17,4 +17,5 @@ def _get_input_from_file():
 
 if __name__ == '__main__':
     path_1, path_2 = _get_input_from_file()
-    print(f'Part 1:  {manhattan_distance(get_closest_shared_point_traversed(path_1, path_2))}')
+    print(f'Part 1:  {manhattan_distance(get_closest_shared_point(path_1, path_2))}')
+    print(f'Part 2:  {get_min_signal_delay(path_1, path_2)}')
