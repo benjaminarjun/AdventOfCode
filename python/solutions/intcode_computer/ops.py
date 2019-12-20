edb7b993d4fb8704ad435d2de036d1942ca93f90
Ben <benarthur91@gmail.com>
2019-12-19 19:34:33 -0800

    Simplify intcode operations: it is stated that the write-to position will never be in immediate mode

--- python/solutions/intcode_computer/ops.py
+++ python/solutions/intcode_computer/ops.py
@@ -8,31 +8,25 @@ class Op:
 
 
 def _intcode_add(program, index, param_modes, debug=False):
-    lh_val_mode, rh_val_mode, target_mode = param_modes
+    lh_val_mode, rh_val_mode, _ = param_modes
 
     lh_val = lh_val_mode == 1 and program[index + 1] or program[program[index + 1]]
     rh_val = rh_val_mode == 1 and program[index + 2] or program[program[index + 2]]
 
     if debug: print(f'Adding {lh_val} and {rh_val}')
 
-    if target_mode == 1:
-        program[index + 3] = lh_val + rh_val
-    else:
-        program[program[index + 3]] = lh_val + rh_val
+    program[program[index + 3]] = lh_val + rh_val
 
 
 def _intcode_multiply(program, index, param_modes, debug=False):
-    lh_val_mode, rh_val_mode, target_mode = param_modes
+    lh_val_mode, rh_val_mode, _ = param_modes
 
     lh_val = lh_val_mode == 1 and program[index + 1] or program[program[index + 1]]
     rh_val = rh_val_mode == 1 and program[index + 2] or program[program[index + 2]]
 
     if debug: print(f'Multiplying {lh_val} and {rh_val}')
 
-    if target_mode == 1:
-        program[index + 3] = lh_val * rh_val
-    else:
-        program[program[index + 3]] = lh_val * rh_val
+    program[program[index + 3]] = lh_val * rh_val
 
 
 op_lookup = {
