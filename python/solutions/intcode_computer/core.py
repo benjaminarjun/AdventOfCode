b1541edc5f943ce52cfec7d8e598e71082b848a9
Ben <benarthur91@gmail.com>
2019-12-19 19:30:48 -0800

    More refactoring and add param mode to intcode computer

--- python/solutions/intcode_computer/core.py
+++ python/solutions/intcode_computer/core.py
@@ -7,26 +7,39 @@ def run_intcode_program(program, debug=False):
     if len(program) == 0:
         raise ValueError('Program cannot be empty.')
 
-    program_list = program.split(',')
+    program_list = [int(z) for z in program.split(',')]
     index = 0
 
-    while int(program_list[index]) != 99:
-        op_id = int(program_list[index])
-        if op_id not in op_lookup.keys():
-            raise ValueError(
-                f'Encountered invalid op ID in program {program}. ' +
-                f'Working version of program: {program_list}, index {index}, val {op_id}. ' +
-                f'Index must be one of: {op_lookup.keys()}'
-            )
-
-        op = op_lookup[op_id]
-
-        if debug: print(f'Applying {op} at index {index}')
-        op.perform(program_list, index, debug)
+    while program_list[index] != 99:
+        # Parse op ID and parameter modes
+        instruction = program_list[index]
+        op, param_modes = _parse_instruction(instruction)
+        if debug: print(f'Applying {op} at index {index} with param modes {param_modes}')
+        op.perform(program_list, index, param_modes, debug)
         index += op.chunk_length
         if debug: print(f'Working copy: {program_list}')
 
-    return ','.join(program_list)
+    return ','.join(map(str, program_list))
+
+
+def _parse_instruction(instruction):
+    instruction_str = str(instruction)
+    op_id = int(instruction_str[-2:])
+
+    if op_id not in op_lookup.keys():
+        raise ValueError(
+            f'Encountered invalid op ID in program {program}. ' +
+            f'Working version of program: {program_list}, index {index}, val {op_id}. ' +
+            f'Index must be one of: {op_lookup.keys()}'
+        )
+
+    op = op_lookup[op_id]
+
+    param_modes = [int(mode) for mode in list(instruction_str[:-2][::-1])]
+    num_addl_modes = op.chunk_length - 1 - len(param_modes) # length of chunk minus 1 for instruction, less num supplied
+    param_modes.extend([0] * num_addl_modes)
+
+    return op, param_modes
 
 
 # TODO maybe does not belong in core
--- python/solutions/intcode_computer/ops.py
+++ python/solutions/intcode_computer/ops.py
@@ -7,22 +7,32 @@ class Op:
         return f'<Op(chunk_length={self.chunk_length}, perform={self.perform.__name__})>'
 
 
-def _intcode_add(program, index, debug=False):
-    lh_val = int(program[int(program[index + 1])])
-    rh_val = int(program[int(program[index + 2])])
+def _intcode_add(program, index, param_modes, debug=False):
+    lh_val_mode, rh_val_mode, target_mode = param_modes
+
+    lh_val = lh_val_mode == 1 and program[index + 1] or program[program[index + 1]]
+    rh_val = rh_val_mode == 1 and program[index + 2] or program[program[index + 2]]
 
     if debug: print(f'Adding {lh_val} and {rh_val}')
 
-    program[int(program[index + 3])] = str(lh_val + rh_val)
+    if target_mode == 1:
+        program[index + 3] = lh_val + rh_val
+    else:
+        program[program[index + 3]] = lh_val + rh_val
 
 
-def _intcode_multiply(program, index, debug=False):
-    lh_val = int(program[int(program[index + 1])])
-    rh_val = int(program[int(program[index + 2])])
+def _intcode_multiply(program, index, param_modes, debug=False):
+    lh_val_mode, rh_val_mode, target_mode = param_modes
+
+    lh_val = lh_val_mode == 1 and program[index + 1] or program[program[index + 1]]
+    rh_val = rh_val_mode == 1 and program[index + 2] or program[program[index + 2]]
 
     if debug: print(f'Multiplying {lh_val} and {rh_val}')
 
-    program[int(program[index + 3])] = str(lh_val * rh_val)
+    if target_mode == 1:
+        program[index + 3] = lh_val * rh_val
+    else:
+        program[program[index + 3]] = lh_val * rh_val
 
 
 op_lookup = {
