7582b6d9bdf789505b98f7504cb89f09cb6ea2e7
Ben <benarthur91@gmail.com>
2019-12-28 11:29:17 -0800

    Day 7 part 1 working

---
+++ python/solutions/day07/results.py
@@ -0,0 +1,18 @@
+from ..aoc_helpers import get_data_file
+from .lib import get_max_pipeline_output
+
+def _get_input_from_file():
+    try:
+        with open(get_data_file('day7_input.txt'), 'r') as f:
+            input_data = f.read()
+    except FileNotFoundError as e:
+        raise Exception('Could not find file.', e)
+
+    return input_data
+
+
+if __name__ == '__main__':
+    program = _get_input_from_file()
+
+    max_output, _ = get_max_pipeline_output(program)
+    print(f'Part 1:  {max_output}')
